6bc72428b1de72a5d02fdb84ab48cd33b2d7fdb0
Georgi Yatsev <giatzev@gmail.com>
2017-10-24 12:15:50 +0300

    initial

---
+++ random-data-generator.js
@@ -0,0 +1,45 @@
+(function () {
+    var data = {};
+    var dataLastSent = '';
+    var listeners = [];
+
+    setInterval(genA, 1500);
+    setInterval(genB, 2270);
+    setInterval(genC, 5234);
+
+    genA();
+    genB();
+    genC();
+
+    function genA() {
+        var time = (new Date()).getTime();
+        data.a = time % 40;
+        pushUpdate();
+    }
+
+    function genB() {
+        var time = (new Date()).getTime();
+        data.b = time % 13;
+        pushUpdate();
+    }
+
+    function genC() {
+        var time = (new Date()).getTime();
+        data.c = time % 2;
+        pushUpdate();
+    }
+
+    function pushUpdate () {
+        var dataString = JSON.stringify(data);
+        if (dataLastSent !== dataString) {
+            listeners.forEach(function (f) {
+                f(data);
+            });
+            dataLastSent = dataString;
+        }
+    }
+
+    window.onRandomData = function (listener) {
+        listeners.push(listener);
+    };
+}());
